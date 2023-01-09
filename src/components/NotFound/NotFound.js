13aa6b1a0ae1511c48b4e1bcb81ba7a96db36e79
Emon02 <abdulalimemon11@gmail.com>
2023-01-09 23:55:22 +0600

    Update 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found</h2>
-        </div>
+        <section className='min-h-[70vh] flex justify-center items-center'>
+            <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div class="w-10/12 mx-auto">
+                    <h2 class="sm:text-4xl text-3xl mb-4 font-medium text-gray-900 text-center">Page not Found
+                    </h2>
+                    <div className='text-center py-4'>
+                    <button className='btn btn-primary text-center'><Link to='/'>Back to Home</Link></button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
