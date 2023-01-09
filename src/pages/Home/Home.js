0df3d3442778c7d39114652f850945571dbe716a
Emon02 <abdulalimemon11@gmail.com>
2023-01-09 23:34:00 +0600

    Tools section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BusinessSummery from './BusinessSummery';
 import HomeBanner from './HomeBanner';
 import Review from './Review';
+import Tools from './Tools';
 
 const Home = () => {
     return (
         <div className='bg-[#FDF7FF]'>
             <div className='container mx-auto'>
                 <HomeBanner></HomeBanner>
+                <Tools></Tools>
                 <BusinessSummery></BusinessSummery>
                 <Review></Review>
             </div>
