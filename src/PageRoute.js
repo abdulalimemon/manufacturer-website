4a7014eb0ac0613c2397a6e07afcab765739316a
Emon02 <abdulalimemon11@gmail.com>
2023-01-11 01:37:22 +0600

    Purchase page

--- src/PageRoute.js
+++ src/PageRoute.js
@@ -5,12 +5,14 @@ import Home from './pages/Home/Home';
 import ForgotPassword from './pages/Login/ForgotPassword';
 import Login from './pages/Login/Login';
 import Register from './pages/Login/Register';
+import Purchase from './pages/Purchase/Purchase';
 
 const PageRoute = () => {
     return (
         <>
             <Routes>
                 <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/purchase' element={<Purchase></Purchase>}></Route>
                
                 <Route path='/login' element={<Login></Login>}></Route>
                 <Route path='/forgotpassword' element={<ForgotPassword></ForgotPassword>}></Route>
