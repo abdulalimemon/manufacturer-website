96e08a6066228c89b61b778b21ea1183d582e779
Emon02 <abdulalimemon11@gmail.com>
2023-01-11 01:15:28 +0600

    Forgot password complete

--- src/pages/Login/ForgotPassword.js
+++ src/pages/Login/ForgotPassword.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import { BiAt } from "react-icons/bi";
+import { toast } from 'react-toastify';
+import Loading from '../../components/Loading/Loading';
+import auth from '../../firebase.init';
 
 const ForgotPassword = () => {
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(
+        auth
+    );
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = async (data) => {
+        console.log(data);
+        await sendPasswordResetEmail(data.email);
+        await toast.success('A reset password email has been sent to your email address. please check your inbox or spam folder.', {
+            theme: "colored",
+        });
+    }
+
+    if (sending) {
+        return <Loading></Loading>
+    }
+
+    if (error?.message) {
+        toast.error('error?.message', {
+            theme: "colored",
+        });
+    }
     return (
         <section className="min-h-[70vh]">
             <div className='container mx-auto'>
@@ -9,7 +36,7 @@ const ForgotPassword = () => {
                     <div className="px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
                         <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl text-center">Forgot Password</h2>
 
-                        <form className="mt-8">
+                        <form onSubmit={handleSubmit(onSubmit)} className="mt-8">
                             <div className="space-y-5">
                                 <div>
                                     <label className="text-base font-medium text-gray-900"> Email address </label>
@@ -19,13 +46,27 @@ const ForgotPassword = () => {
                                         </div>
 
                                         <input
-                                            type="email"
-                                            name=""
-                                            id=""
+                                            type="email" id="email"
                                             placeholder="Enter email address"
                                             className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
-                                        />
+                                            aria-invalid={errors.email ? "true" : "false"}
+                                            {...register('email', {
+                                                required: {
+                                                    value: true,
+                                                    message: 'Email is Required'
+                                                },
+                                                pattern: {
+                                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                                    message: 'Provide a valid email address.'
+                                                }
+                                            })} />
                                     </div>
+                                    <label className="label pb-0">
+                                        <label className="label">
+                                            {errors.email?.type === 'required' && <span className="label-text-alt text-red-600 font-semibold">{errors.email.message}</span>}
+                                            {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-600 font-semibold">{errors.email.message}</span>}
+                                        </label>
+                                    </label>
                                 </div>
                                 <div>
                                     <button
